7a703d3ac33a0eb32d7cc3613e5075bcbbf18f2e
michalkruk <michal.k.kruk@gmail.com>
2019-11-19 22:11:02 +0100

    up

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h3>Add Todo</h3>
+        <div class="add form-group text-center">
+            <form @submit="onSubmit">
+                <input
+                    class="form-control"
+                    type="text"
+                    v-model="title"
+                    placeholder="Add todo"
+                />
+                <input class="btn btn-primary" type="submit" value="Submit" />
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+    name: 'AddTodo',
+    data() {
+        return { title: '' };
+    },
+    methods: {
+        ...mapActions(['addTodo']),
+        onSubmit(e) {
+            e.preventDefault();
+            this.addTodo(this.title);
+        }
+    }
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h3>Todos</h3>
+        <div class="card-columns">
+            <div
+                v-for="todo in allTodos"
+                :key="todo.id"
+                class="card text-white bg-primary p-4 mb-3"
+                style="max-width: 20rem;"
+            >
+                <div class="class-text text-center">
+                    {{ todo.title }}
+                    <i
+                        @click="deleteTodo(todo.id)"
+                        class="fas fa-trash-alt"
+                    ></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+export default {
+    name: 'Todos',
+    methods: {
+        ...mapActions(['fetchTodos', 'deleteTodo'])
+    },
+    computed: mapGetters(['allTodos']),
+    created() {
+        this.fetchTodos();
+    }
+};
+</script>
+
+<style scoped>
+i {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+    cursor: pointer;
+}
+</style>
